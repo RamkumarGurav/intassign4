2493246984379ce789250a9f24323801a5d36016
ramkumar gurav <ramkumarsgurav@gmail.com>
2024-04-29 12:00:03 +0530

    final commit

--- app/(view)/(auth)/login/page.tsx
+++ app/(view)/(auth)/login/page.tsx
@@ -5,9 +5,14 @@ export default function InputForm() {
     <section
       id="Section"
       className={`bg-gray-300 py-[35px] sm:py-[50px] sm:px-[35px] xl:px-[70px] min-h-screen  
-      |||  flex justify-center items-center`}
+      |||  flex flex-col justify-center items-center`}
     >
       <title>Login</title>
+      <div className="flex gap-1 bg-white p-2 mb-4 rounded-full flex-col justify-center items-center">
+        <p className="font-bold underline">test credentials</p>
+        <p> username:Hukum Gupta</p>
+        <p>password:hukum1234</p>
+      </div>
       <LoginCard />
     </section>
   );
